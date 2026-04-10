18572c5c0be9b1c098d7e0baf6f0acd90d347835
Austin YikNing <austin@ajobthing.com>
2026-04-10 12:03:23 +0800

    fix: bypass TypeScript error for Promise.withResolvers and Object.hasOwn

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Polyfill Promise.withResolvers for older browsers (like Safari < 17.4) - requested by pdfjs-dist v4+
-if (typeof Promise.withResolvers === 'undefined') {
-  // @ts-expect-error polyfill
-  Promise.withResolvers = function () {
-    let resolve, reject;
+if (typeof (Promise as any).withResolvers === 'undefined') {
+  (Promise as any).withResolvers = function () {
+    let resolve: any, reject: any;
     const promise = new Promise((res, rej) => { resolve = res; reject = rej; });
     return { promise, resolve, reject };
   };
 }
 
 // Polyfill Object.hasOwn for older browsers (Safari < 15.4)
-if (!Object.hasOwn) {
-  Object.hasOwn = function (obj, prop) {
+if (!(Object as any).hasOwn) {
+  (Object as any).hasOwn = function (obj: any, prop: any) {
     return Object.prototype.hasOwnProperty.call(obj, prop);
   };
 }
