fa62cafb69855cb9aa2462b750c306834555b735
Austin YikNing <austin@ajobthing.com>
2026-04-10 11:57:13 +0800

    fix: polyfill Promise.withResolvers and Object.hasOwn for older Safari/browsers (pdfjs-dist support)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Polyfill Promise.withResolvers for older browsers (like Safari < 17.4) - requested by pdfjs-dist v4+
+if (typeof Promise.withResolvers === 'undefined') {
+  // @ts-expect-error polyfill
+  Promise.withResolvers = function () {
+    let resolve, reject;
+    const promise = new Promise((res, rej) => { resolve = res; reject = rej; });
+    return { promise, resolve, reject };
+  };
+}
+
+// Polyfill Object.hasOwn for older browsers (Safari < 15.4)
+if (!Object.hasOwn) {
+  Object.hasOwn = function (obj, prop) {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+  };
+}
+
 import App from './App.tsx';
 import './index.css';
 
